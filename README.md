0396e0f495c699eef8482f87c2a2029a41904068
forumwt <16545063+forumwt@users.noreply.github.com>
2023-12-28 23:58:52 +0100

    Added new features.

--- README.md
+++ README.md
@@ -6,29 +6,31 @@ Enhance your UI experience by applying a blur effect to the menu with this exten
 
 ### Installation Steps
 1. Activate the command palette using `Ctrl + Shift + P`.
-2. Type "Blur Menu - Enable" and select it.
+2. Type "Blur Menu - Enable Blur" and select it.
 3. Input the desired blur level (16 is recommended).
-4. Restart Visual Studio Code to apply changes.
-5. Navigate to `settings.json` in VS Code.
-6. Incorporate the following transparency settings for your theme:
+4. (Optional) Enable animations in VSCode
+5. Restart Visual Studio Code to apply changes.
+6. Navigate to `settings.json` in VS Code.
+7. Incorporate the following transparency settings for your theme:
 
 ```json
 "workbench.colorCustomizations": {
     "menu.background": "#2e333b70", //context menu
     "menu.selectionBackground": "#4247509a", //context menu hover
     "editorWidget.background": "#51586320", //widget and tooltip
-    "quickInput.background": "#2e333b00" //command center drop down menu
+    "quickInput.background": "#2e333b00", //command center drop down menu
+    "notifications.background": "#29303bd7", //Notification background color.
 }
 ```
 
-> **Tip:** The above configuration is tailored for the [Dreamweaver Dark Theme](https://marketplace.visualstudio.com/items?itemName=Gaga-Dev.dreamweaver-dark-theme).
+> **Tip:** The above configuration is tailored for the [Dreamweaver Dark Theme](https://marketplace.visualstudio.com/items?itemName=Gaga-Dev.dreamweaver-dark-theme) you can setup color for your theme as you like.
 
 ### Removal Process
 1. Open the command palette with `Ctrl + Shift + P`.
-2. Search and execute "Blur Menu - Disable & Restore system setting".
+2. Search and execute "Blur Menu - Disable & Restore setting".
 3. Restart VS Code to revert to default settings.
 
 ## Preview
-
-### Dreamweaver Theme UI
 ![UI Preview](images/image.png "User Interface with Blur Effect")
+![UI Preview](images/animation.gif "Animation User Interface with Blur Effect")
+
--- extension.js
+++ extension.js
@@ -22,6 +22,24 @@ const workbenchJsPath = path.join(
 	"workbench.desktop.main.js"
 );
 
+async function promptRestart(msg) {
+	const config = vscode.workspace.getConfiguration();
+	const configKey = "update.mode";
+	const value = config.inspect(configKey);
+
+	await config.update(
+		configKey,
+		config.get(configKey) === "default" ? "manual" : "default",
+		vscode.ConfigurationTarget.Global
+	);
+	config.update(
+		configKey,
+		value?.globalValue,
+		vscode.ConfigurationTarget.Global
+	);
+	vscode.window.showInformationMessage(msg);
+}
+
 function applyChecksum() {
 	const product = require(productFile);
 	let changed = false;
@@ -83,17 +101,15 @@ function createBackup(filePath) {
 	}
 }
 
-function restoreFromBackup(filePath,msgShow = true) {
+function restoreFromBackup(filePath, msgShow = true) {
 	const backupPath = `${filePath}.backup`;
 	if (fs.existsSync(backupPath)) {
 		fs.copyFileSync(backupPath, filePath);
-		if(msgShow){
-			vscode.window.showInformationMessage(
-				`Settings restored, Restart VS Code to see changes.`
-			);
+		if (msgShow) {
+			promptRestart("Settings restored. Restart VS Code to see changes.");
 		}
 	} else {
-		if(msgShow){
+		if (msgShow) {
 			vscode.window.showInformationMessage(`Backup files not found`);
 		}
 	}
@@ -101,6 +117,7 @@ function restoreFromBackup(filePath,msgShow = true) {
 function activate(context) {
 	const changeSystem = "blur-menu.modifyFiles";
 	const restoreSystem = "blur-menu.restoreSettings";
+	let newAnimation = "";
 	const modifyDisposable = vscode.commands.registerCommand(
 		changeSystem,
 		async () => {
@@ -112,34 +129,47 @@ function activate(context) {
 				vscode.window.showInformationMessage(
 					"No blur level entered. No changes applied."
 				);
-			} else {
-				restoreFromBackup(workbenchCssPath,false);
-				createBackup(workbenchCssPath);
-				createBackup(workbenchJsPath);
-				
-				const newLineContent =
-					`.action-widget:after,.suggest-details-container:after,.context-view.top.left:after,.overflowingContentWidgets>div:after,.workbench-hover-container:after,.find-widget:after,.monaco-menu:after,.shadow-root-host::part(menu)::after{z-index:-1;content:'';position:absolute;left:0;top:0;bottom:0;right:0;backdrop-filter:blur(`+blurThreshold+`px)}.quick-input-widget{backdrop-filter:blur(`+blurThreshold+`px)}`;
-				// Update workbench.desktop.main.css file
-				const cssFileContent = fs.readFileSync(workbenchCssPath,"utf-8");
-				const modifiedCssContent = cssFileContent + newLineContent;
-				fs.writeFileSync(workbenchCssPath, modifiedCssContent, "utf-8");
-
-				// Update workbench.desktop.main.js file
-				const jsFileContent = fs.readFileSync(workbenchJsPath, "utf-8");
-				if (!jsFileContent.includes('T.classList.add("monaco-menu"),T.setAttribute("role","presentation"),T.setAttribute("part","menu")')) {
-					const modifiedJsContent = jsFileContent.replace(
-						/T\.classList\.add\("monaco-menu"\),T\.setAttribute\("role","presentation"\)/g,
-						'T.classList.add("monaco-menu"),T.setAttribute("role","presentation"),T.setAttribute("part","menu")'
-					);
-					fs.writeFileSync(workbenchJsPath, modifiedJsContent, "utf-8");
-				};
-				
-				cleanupOrigFiles();
-				applyChecksum();
-				vscode.window.showInformationMessage(
-					"Restart VS Code to see changes."
-				);
+				return;
 			}
+			const vscodeAnimation = await vscode.window.showQuickPick(
+				["Yes", "No"],
+				{
+					placeHolder: "Enable Animation in VSCode?",
+				}
+			);
+
+			if (vscodeAnimation === "Yes") {
+				newAnimation = `.window-appicon{display:none}.monaco-menu .monaco-action-bar.vertical .action-label,.monaco-menu .monaco-action-bar.vertical .keybinding{height:2em!important;line-height:2em!important}.monaco-icon-label:before{background-size:18px!important;width:18px!important;padding-right:4px!important}.tree-explorer-viewlet-tree-view .monaco-tl-twistie:not(.force-twistie){background-image:none!important;width:0!important;padding-right:0!important;visibility:hidden}.scrollbar.vertical{width:4px!important}.monaco-list .monaco-list-rows{background:0 0!important}.lightBulbWidget{transition:top .2s}.monaco-hover:not(.hidden){animation:.2s hoverFadeIn}@keyframes hoverFadeIn{from{opacity:0}to{opacity:1}}.action-label{transition:color .2s}.monaco-workbench .split-view-view:has(.part.editor),.monaco-workbench .split-view-view:has(.part.sidebar){transition-property:top,left,width,height;transition-duration:.2s}.monaco-workbench:has(iframe) .split-view-view:has(.part.editor),.monaco-workbench:has(iframe) .split-view-view:has(.part.sidebar){transition:none!important}.monaco-workbench .split-view-view:not(.visible):has(.part.sidebar){display:block!important;opacity:0}.quick-input-widget{transform-origin:top;animation:.4s openPopup}.quick-input-widget[style*="display: none;"]{display:block!important;transform-origin:top;animation:.4s closePopup;opacity:0;transform:scaleY(0);pointer-events:none}@keyframes openPopup{from{opacity:0;transform:scaleY(0)}to{opacity:1;transform:scaleY(1)}}@keyframes closePopup{from{opacity:1;transform:scaleY(1)}to{opacity:0;transform:scaleY(0)}}.monaco-list-row:not(.explorer-folders-view .monaco-list-row){animation:.4s scrollingAnimation}@keyframes scrollingAnimation{from{opacity:0}to{opacity:1}}`;
+			}
+
+			restoreFromBackup(workbenchCssPath, false);
+			createBackup(workbenchCssPath);
+			createBackup(workbenchJsPath);
+
+			const newLineContent =
+				`.action-widget:after,.suggest-details-container:after,.context-view.top.left:after,.overflowingContentWidgets>div:after,.workbench-hover-container:after,.find-widget:after,.monaco-menu:after,.shadow-root-host::part(menu)::after{z-index:-1;content:'';position:absolute;left:0;top:0;bottom:0;right:0;backdrop-filter:blur(`+ blurThreshold +`px)}.quick-input-widget{backdrop-filter:blur(`+ blurThreshold +`px)}`+ newAnimation +``;
+			// Update workbench.desktop.main.css file
+			const cssFileContent = fs.readFileSync(workbenchCssPath, "utf-8");
+			const modifiedCssContent = cssFileContent + newLineContent;
+			fs.writeFileSync(workbenchCssPath, modifiedCssContent, "utf-8");
+
+			// Update workbench.desktop.main.js file
+			const jsFileContent = fs.readFileSync(workbenchJsPath, "utf-8");
+			if (
+				!jsFileContent.includes(
+					'T.classList.add("monaco-menu"),T.setAttribute("role","presentation"),T.setAttribute("part","menu")'
+				)
+			) {
+				const modifiedJsContent = jsFileContent.replace(
+					/T\.classList\.add\("monaco-menu"\),T\.setAttribute\("role","presentation"\)/g,
+					'T.classList.add("monaco-menu"),T.setAttribute("role","presentation"),T.setAttribute("part","menu")'
+				);
+				fs.writeFileSync(workbenchJsPath, modifiedJsContent, "utf-8");
+			}
+
+			cleanupOrigFiles();
+			applyChecksum();
+			promptRestart("You must restart VS Code to see changes");
 		}
 	);
 
@@ -154,5 +184,4 @@ function activate(context) {
 	);
 	context.subscriptions.push(modifyDisposable, restoreDisposable);
 }
-
 exports.activate = activate;
